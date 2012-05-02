88d71f842862cd2f97bafa456ae1c81fde1bbd1b
Loic Cimon <loic.cimon@laposte.net>
2012-05-02 22:58:39 +0200

    Creation fenetre du choix du nombre de joueurs

---
+++ src/Vue/Fenetre.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Loïc Cimon
+ */
+public class Fenetre extends JFrame {
+
+    public Fenetre() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Caylus");
+        this.setContentPane(new choixJoueurPanel());
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setResizable(false);
+    }
+
+    public void affichePlateau() {
+        this.setContentPane(new Plateau());
+        //this.setPreferredSize(new Dimension(505, 695));
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Fenetre();
+    }
+}
---
+++ src/Vue/Plateau.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import Vue.Outils.ImagePanel;
+
+/**
+ *
+ * @author Loïc Cimon
+ */
+public class Plateau extends ImagePanel {
+
+//    private Jeu jeu;
+
+    public Plateau() {
+        super("Images/3070_1.jpg");
+
+        initComponents();
+    }
+
+    private void initComponents() {
+
+    }
+}
